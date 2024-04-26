d0f371e1102f3270ae8352fff338bf394938355f
JarkkoKarki <karkijarkko@gmail.com>
2024-04-26 14:53:37 +0300

    Nappulat tehty ja lentokentät haettu tietokannasta -> js

--- Frontend/Test.JS.js
+++ Frontend/Test.JS.js
@@ -5,7 +5,10 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60, 24], 7);
+map.setView([60, 24], 5);
+
+const blueIcon = L.divIcon({className: 'blue-icon'})
+const greenIcon = L.divIcon({className: 'green-icon'})
 
 
 document.querySelector('#player-form').addEventListener('submit', function (evt) {
